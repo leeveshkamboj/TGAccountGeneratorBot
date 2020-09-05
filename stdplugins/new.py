9b5ef74fb4ebba05927f833b6e7b9c911164f7b3
Leevesh Kamboj <leeveshk12@gmail.com>
2020-09-05 10:55:29 +0530

    make it better

--- stdplugins/new.py
+++ stdplugins/new.py
@@ -3,6 +3,6 @@ from telethon import events
 @borg.on(events.NewMessage)
 async def my_event_handler(event):
     if not event.is_channel and '/start' in event.raw_text.lower():
-        await borg.send_message(event.chat_id, "For @PandaZnetwork.\n\n**Made by @HeisenbergTheDanger**")
+        await borg.send_message(event.chat_id, "**Made by @HeisenbergTheDanger for @PandaZnetwork.**")
     elif not event.is_channel and 'yo' in event.raw_text.lower():
         await event.reply('yo')
