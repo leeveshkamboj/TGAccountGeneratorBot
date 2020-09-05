03c61374321d001bff0c1865038345b2a1c07cf0
Leevesh Kamboj <leeveshk12@gmail.com>
2020-09-05 09:56:57 +0530

    make it better

--- stdplugins/new.py
+++ stdplugins/new.py
@@ -2,5 +2,7 @@ from telethon import events
 
 @borg.on(events.NewMessage)
 async def my_event_handler(event):
-    if not event.is_channel and 'yo' in event.raw_text.lower():
+    if not event.is_channel and '/start' in event.raw_text.lower():
+        await borg.send_message(event.chat_id, "**Made by @HeisenbergTheDanger**")
+    elif not event.is_channel and 'yo' in event.raw_text.lower():
         await event.reply('yo')
