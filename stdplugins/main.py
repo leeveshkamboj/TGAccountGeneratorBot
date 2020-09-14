4ffa5de7e91bfac7db21b7cb0952c35c18413448
Leevesh Kamboj <leeveshk12@gmail.com>
2020-09-14 13:34:34 +0530

    make it better

--- stdplugins/main.py
+++ stdplugins/main.py
@@ -6,15 +6,15 @@ channel_id = -1001481026778
 msg_id = 211
 sticker_delete = False
 footer = "\n\n**__➖🔰@PandaZnetwork🔰➖__**"
-img = {-1001481026778 : "https://i.imgur.com/fQi4wJe.jpg", -1001481899343 : "https://i.imgur.com/DRUnSIc.jpg", -1001122798596 : "https://i.imgur.com/mGgAIbl.jpg", -1001251394025 : "https://i.imgur.com/NG6M6Eh.jpg", -1001351480003 : "https://i.imgur.com/rhXRIKw.jpg", -1001177011841 : "https://i.imgur.com/tL2awKR.jpg"}
-name = {-1001481026778 : "Express VPN", -1001481899343 : "Windscribe", -1001122798596 : "IP Vanish", -1001251394025 : "Hulu", -1001351480003 : "DisneyPlus", -1001177011841 : "Nord VPN"}
+img = {-1001481026778 : "https://i.imgur.com/fQi4wJe.jpg", -1001481899343 : "https://i.imgur.com/DRUnSIc.jpg", -1001122798596 : "https://i.imgur.com/mGgAIbl.jpg", -1001251394025 : "https://i.imgur.com/NG6M6Eh.jpg", -1001351480003 : "https://i.imgur.com/rhXRIKw.jpg", -1001177011841 : "https://i.imgur.com/tL2awKR.jpg", -1001313593468 : "https://i.imgur.com/UnUErxw.jpg"}
+name = {-1001481026778 : "Express VPN", -1001481899343 : "Windscribe", -1001122798596 : "IP Vanish", -1001251394025 : "Hulu", -1001351480003 : "DisneyPlus", -1001177011841 : "Nord VPN", -1001313593468 : "McAfee"}
 
 
 @borg.on(events.NewMessage)
 async def my_event_handler(event):                     
     global channel_id, msg_id, sticker_delete, footer, img
     if channel_id is not None and msg_id is not None and event.chat_id in img.keys():
-        if event.sticker is not None:
+        if event.sticker:
             if sticker_delete:
                 await event.delete()
         elif event.gif is not None or event.poll is not None:
