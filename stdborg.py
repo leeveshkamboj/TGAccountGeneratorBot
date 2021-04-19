729543cfd2ab972027627b675ae4241f76d34872
Leevesh Kamboj <leeveshk12@gmail.com>
2021-04-19 17:37:38 +0530

    make it better

--- stdborg.py
+++ stdborg.py
@@ -1,12 +1,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import os
+
+
+class Config(object):
+    LOGGER = True
+    # Get this value from my.telegram.org! Please do not steal
+    TOKEN = os.environ.get("TOKEN", None)
+    DB_URI = os.environ.get("DATABASE_URL", None)
+    
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
+
 
 import logging
 
 from uniborg import Uniborg
-import api_key
-import os
 
 logging.basicConfig(level=logging.INFO)
 
