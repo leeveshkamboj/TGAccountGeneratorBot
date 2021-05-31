9b457a25c472ed93b26902438b573c762c732ba3
Leevesh Kamboj <leeveshk12@gmail.com>
2021-05-31 18:00:59 +0530

    make it better

--- stdplugins/sql_helpers/users_sql.py
+++ stdplugins/sql_helpers/users_sql.py
@@ -52,6 +52,6 @@ def resetDailyLimit():
     return True
 
 def exceededLimitUsers(dailyLimit):
-    rem = SESSION.query(userlist).filter(userlist.dailylimit == dailyLimit).all()
+    rem = SESSION.query(userlist).filter(userlist.dailylimit == str(dailyLimit)).all()
     SESSION.close()
     return rem
