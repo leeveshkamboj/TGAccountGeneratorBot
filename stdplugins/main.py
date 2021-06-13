e2d1eb934fc41dc64ca3ac0b594131b88af5fbb5
Leevesh Kamboj <leeveshk12@gmail.com>
2021-06-13 21:19:06 +0530

    make it better

--- stdplugins/main.py
+++ stdplugins/main.py
@@ -7,6 +7,7 @@ from stdplugins.sql_helpers.users_sql import get_user, add_user, get_all_users, 
 from stdplugins.sql_helpers.hits_sql import hitExists, addHit, remHit, get_all_hits, get_hit_by_id
 import io
 import requests
+import re
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
