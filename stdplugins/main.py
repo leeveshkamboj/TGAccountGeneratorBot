1b6fd87fef81c06d2b3f019e25a4bb3bfae18caf
Leevesh Kamboj <leeveshk12@gmail.com>
2021-06-13 22:44:03 +0530

    make it better

--- stdplugins/main.py
+++ stdplugins/main.py
@@ -194,7 +194,9 @@ Do /gen to generate an account
                         hit = hit.strip()
                         hit = hit.split(" ")[0]
                         if ":" in hit:
-                            print(hit)
+                            if hit[0] == "[":
+                                mail, pwd = hit.split(":", maxsplit = 2)
+                                hit = mail[mail.index("[") + 1 : mail.index("]")] + ":" + pwd
                             hitID = hitExists(hit).hitID
                             if hitID:
                                 remHit(hitID)
