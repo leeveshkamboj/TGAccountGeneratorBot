5d1384011f9f3ec3b7e49015ad3ffe7d21b80e7c
Leevesh Kamboj <leeveshk12@gmail.com>
2021-06-14 13:20:40 +0530

    make it better

---
+++ uniborg/config.py
@@ -0,0 +1,13 @@
+import os
+class Var(object):
+    ownerIDs = os.environ.get("OWNER_IDS", [])
+    if ownerIDs:
+        ids = ownerIDs.split("|")
+        ownerIDs = [int(ID) for ID in ids]
+    channelId = int(os.environ.get("CHANNEL_ID", 0))
+    groupId = int(os.environ.get("GROUP_ID", 0))
+    repotgroupId = int(os.environ.get("REPORT_GROUP_ID", 0))
+    channelName = os.environ.get("CHANNEL_NAME", '')
+    hitChannelId = int(os.environ.get("HIT_CHANNEL_ID", 0))
+    dailyLimit = int(os.environ.get("DAILY_LIMIT", 3))
+    maintenanceMode = bool(os.environ.get("MAINTENANCE_MODE", False))
