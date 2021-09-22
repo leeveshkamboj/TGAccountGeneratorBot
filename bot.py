206aebfd4b8ae39531ce7a416653f4c1aa1f4906
Leevesh Kamboj <leeveshk12@gmail.com>
2021-09-22 09:53:30 +0530

    make it better

--- bot.py
+++ bot.py
@@ -112,7 +112,7 @@ async def my_event_handler(event):
                 [Button.url("Authentication error?", "https://t.me/nordbypass")],
                 [(Button.inline("Report not working", data=f"report_{hitID}"))]
             ]
-            await bot.send_message(event.chat_id, genMsg.format(email = hit[0], pwd = hit[1], name = first_name, channelName = channelName), buttons = button)
+            await bot.send_message(event.chat_id, genMsg.format(email = hit[0], pwd = hit[1], name = first_name, channelName = Var.channelName), buttons = button)
         else:
             await bot.send_message(event.chat_id, "No account available right now.")
     if '/start' == event.raw_text.lower():
